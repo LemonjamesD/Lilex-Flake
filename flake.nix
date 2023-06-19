e8a5be358561350aa33ce6af2fa0cb12d35777c3
LemonjamesD <lemon@lemonjamesd.com>
2023-06-19 19:04:56 -0400

    Precompiled Font

--- flake.nix
+++ flake.nix
@@ -15,20 +15,14 @@
       defaultPackage = pkgs.stdenv.mkDerivation {
         name = "lilex-font";
         version = "2.200";
-        src = builtins.fetchGit {
-          url = "https://github.com/mishamyrt/Lilex";
-          rev = "6de21bfa221564e8694f281c663f16ab93b76c33";
+        src = pkgs.fetchzip {
+          url = "https://github.com/mishamyrt/Lilex/releases/download/2.200/Lilex.zip";
+          sha256 = "sha256-MPQfqCMFMjcAlMos1o4bC+I+cvQYwr2TjI4Q03QeuaQ=";
+          stripRoot = false;
         };
-        buildInputs = with pkgs; [
-          python311Packages.setuptools
-          ttfautohint
-          gcc # build-essential
-          libffi
-          libgit2
-        ];
-        buildPhase = ''
-          cd $src
-          # make configure doesn't work bruh
+        installPhase = ''
+          mkdir -p $out/share/fonts
+          cp -R $src $out/share/fonts/opentype/
         '';
         meta = { description = "Lilex Font."; };
       };
